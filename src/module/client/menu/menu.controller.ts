7b2db4a17546dcffb358b036b587a3325e250ecf
H-Q-V <quangvinh06092003@gmail.com>
2024-10-02 01:17:03 +0700

    new

---
+++ src/module/client/menu/menu.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { MenuService } from './menu.service';
+import { CreateMenuDto } from './menu.dto';
+
+@Controller('menu')
+export class MenuController {
+  constructor(private menuService: MenuService) {}
+
+  @Post('/create')
+  @UsePipes(new ValidationPipe())
+  async createMenu(@Body() createMenuDto: CreateMenuDto) {
+    let data = await this.menuService.create(createMenuDto);
+    return {
+      success: true,
+      code: 200,
+      message: 'create menu successfuly',
+      data,
+    };
+  }
+
+  @Put('/update/:id')
+  @UsePipes(new ValidationPipe())
+  async update(@Param('id') id: string, @Body() body) {
+    let data = await this.menuService.update(id, body);
+    return {
+      success: true,
+      code: 200,
+      message: 'update menu successfuly',
+      data,
+    };
+  }
+
+  @Delete('/delete/:id')
+  @UsePipes(new ValidationPipe())
+  async delete(@Param('id') id: string) {
+    await this.menuService.delete(id);
+    return {
+      success: true,
+      code: 200,
+      message: 'delete menu successfuly',
+    };
+  }
+
+  @Get('getAll')
+  async getMenus() {
+    let menus = await this.menuService.getMenus();
+    return {
+      success: true,
+      code: 200,
+      data: menus,
+    };
+  }
+}
