8ab2b923d7e01251a5e6988476e3a65dee47b31f
nguyenviet2003 <130578886+vietnhat0607@users.noreply.github.com>
2024-10-02 01:35:10 +0700

    xóa

--- src/module/client/menu/menu.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateMenuDto {
-  @IsString()
-  @IsNotEmpty()
-  name?: string;
-}
--- src/schema/menu.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-
-@Schema()
-export class Menu {
-  @Prop()
-  name: string;
-}
-
-export const MenuSchema = SchemaFactory.createForClass(Menu);
