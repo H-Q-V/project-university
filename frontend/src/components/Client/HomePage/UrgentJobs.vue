77092ec8929eaa2494c16c9c393fbdf40f0e38d9
nguyenviet2003 <130578886+vietnhat0607@users.noreply.github.com>
2024-10-09 22:29:59 +0700

    front-end

---
+++ frontend/src/components/Client/HomePage/UrgentJobs.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="col-md-8 col-sm-12 col-12">
+    <div class="job-board-wrap">
+      <h2 class="widget-title">
+        <span>Tuyển gấp</span>
+      </h2>
+
+      <div class="job-group">
+        <div v-for="job in urgentJobs" :key="job.id" class="job pagi">
+          <div class="job-content">
+            <div class="job-logo">
+              <a :href="job.companyLink">
+                <img :src="job.logo" class="job-logo-ima" :alt="job.company + ' logo'"> 
+              </a>
+            </div>
+
+            <div class="job-desc">
+              <div class="job-title">
+                <a :href="job.link">{{ job.title }}</a>
+              </div>
+              <div class="job-company">
+                <a :href="job.companyLink">{{ job.company }}</a> | 
+                <a :href="job.link" class="job-address">
+                  <i class="fa fa-map-marker" aria-hidden="true"></i>
+                  {{ job.location }}
+                </a>
+              </div>
+
+              <div class="job-inf">
+                <div class="job-main-skill">
+                  <i class="fa fa-code" aria-hidden="true"></i>
+                  <a :href="job.skillLink"> {{ job.mainSkill }}</a>
+                </div>
+                <div class="job-salary">
+                  <i class="fa fa-money" aria-hidden="true"></i>
+                  <span class="salary-min">{{ job.salaryMin }}<em class="salary-unit">triệu</em></span>
+                  <span class="salary-max">{{ job.salaryMax }} <em class="salary-unit">triệu</em></span>
+                </div>
+                <div class="job-deadline">
+                  <span><i class="fa fa-clock-o" aria-hidden="true"></i> Hạn nộp: <strong>{{ job.deadline }}</strong></span>
+                </div>
+              </div>
+            </div>
+            <div class="wrap-btn-appl">
+              <a :href="job.applyLink" class="btn btn-appl">Ứng tuyển ngay</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="readmorestyle-wrap">
+        <a href="#" class="readallstyle reads1">Xem tất cả</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UrgentJobs',
+      
+  data() {
+    return {
+      urgentJobs: [
+        {
+          id: 1,
+          title: 'Lập trình viên Frontend',
+          company: 'Công ty ABC',
+          logo: '/src/assets/img/fpt-logo.png', 
+          link: '#',
+          companyLink: '#',
+          location: 'Hà Nội',
+          skillLink: '#',
+          mainSkill: 'Vue.js',
+          salaryMin: 15,
+          salaryMax: 25,
+          deadline: '30/06/2023',
+          applyLink: '#'
+        },
+        {
+          id: 2,
+          title: 'Kỹ sư phần mềm Backend',
+          company: 'Công ty XYZ',
+          logo: '/src/assets/img/fpt-logo.png', 
+          link: '#',
+          companyLink: '#',
+          location: 'Hồ Chí Minh',
+          skillLink: '#',
+          mainSkill: 'Node.js',
+          salaryMin: 20,
+          salaryMax: 30,
+          deadline: '15/07/2023',
+          applyLink: '#'
+        },
+        {
+          id: 2,
+          title: 'Kỹ sư phần mềm Backend',
+          company: 'Công ty XYZ',
+          logo: '/src/assets/img/fpt-logo.png', 
+          link: '#',
+          companyLink: '#',
+          location: 'Hồ Chí Minh',
+          skillLink: '#',
+          mainSkill: 'Node.js',
+          salaryMin: 20,
+          salaryMax: 30,
+          deadline: '15/07/2023',
+          applyLink: '#'
+        },
+      ]
+    }
+  }, 
+
+  created() {
+    console.log('Danh sách công việc khẩn cấp đã được tải');
+  }
+}
+</script>
+
+<style scoped>
+.job-board-wrap {
+    margin-top: 30px; 
+}
+</style>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,7 +12,9 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'jquery': resolve(__dirname, 'node_modules/jquery/dist/jquery.js') // Sử dụng dist thay vì src
+
     }
   }
 })
